cc21c8ef404577d0da946ecd9bd277dd6dc69701
saravanankuppusamy <saravanan_kuppusamy@yahoo.com>
2021-03-02 11:36:25 -0500

    Grdle init config added jwt dependencies

---
+++ mcc-project-data/src/main/java/com/msd/mcc/MccProjectDataApplication.java
@@ -0,0 +1,13 @@
+package com.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccProjectDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccProjectDataApplication.class, args);
+	}
+
+}
---
+++ mcc-project-data/src/test/java/com/msd/mcc/MccProjectDataApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.msd.mcc;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MccProjectDataApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
